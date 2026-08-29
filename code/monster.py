84d75c24d132cec1d399ba74cc1a27e8c605d748
bangzishu-huang <bangzishu@gmail.com>
2026-08-29 14:46:26 -0500

    base setup coding for the game

---
+++ code/monster.py
@@ -0,0 +1,8 @@
+from settings import *
+
+class Monster(pygame.sprite.Sprite):
+    def __init__(self, name, surf) :
+        super().__init__()
+        self.image = surf
+        self.rect = self.image.get_frect(bottomleft = (100, WINDOW_HEIGHT))
+        self.name = name
